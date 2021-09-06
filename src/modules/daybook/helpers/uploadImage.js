b8ca11886ba04d32146ed27e0c7d30bbf7e7e142
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2021-09-06 18:11:27 -0500

    CRUD

---
+++ src/modules/daybook/helpers/uploadImage.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+
+const uploadImage = async (file) =>{
+    if (!file)return
+    
+    try {
+        const formData = new FormData()
+        formData.append("upload_preset", "curso-vue")
+        formData.append("file", file)
+
+        const url = "https://api.cloudinary.com/v1_1/dkekcqpva/image/upload"
+        const {data} = await axios.post(url, formData)
+        console.log(data)
+
+        return data.secure_url
+    } catch (error) {
+        console.error("Error al cargar imagen")
+        console.error("El error es el siguiente", error)
+        return null
+    }
+}
+
+export default uploadImage
+
--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,23 +1,4 @@
 export default () => ({
     isLoading: true,
-    entries: [
-        {
-            id: new Date().getTime(),
-            date: new Date().toDateString(),
-            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias magnam, iure exercitationem magni recusandae reprehenderit. Consectetur laudantium, quibusdam aliquam nisi minima temporibus id recusandae perspiciatis impedit repellendus consequatur culpa quae.",
-            picture: null
-        },
-        {
-            id: new Date().getTime() + 1000,
-            date: new Date().toDateString(),
-            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias magnam, iure exercitationem magni recusandae reprehenderit. Consectetur laudantium, quibusdam aliquam nisi minima temporibus id recusandae perspiciatis impedit repellendus consequatur culpa quae.",
-            picture: null
-        },
-        {
-            id: new Date().getTime() + 2000,
-            date: new Date().toDateString(),
-            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias magnam, iure exercitationem magni recusandae reprehenderit. Consectetur laudantium, quibusdam aliquam nisi minima temporibus id recusandae perspiciatis impedit repellendus consequatur culpa quae.",
-            picture: null
-        },
-    ]
+    entries: []
 })
